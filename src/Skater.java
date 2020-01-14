47edd638850673651139c38b2e32d653bf6c49f3
minaker-l8 <minaker-l8@142.132.136.210>
2020-01-14 11:19:21 -0600

    First commit

---
+++ src/Skater.java
@@ -0,0 +1,38 @@
+/********************************************************************
+ * Programmer: Lauren
+ * 
+ * Class:  CS30S
+ *
+ * Assignment: Skater class
+ *
+ * Description: Skater object with a unique ID and stats.
+ ***********************************************************************/
+
+// import libraries as needed here
+
+public class Skater {
+    //*** Class Variables ***
+    
+        public static int nextId = 1000; // static variable to get next ID
+    
+    //*** Instance Variables ***
+    
+    //*** Constructors ***
+    
+    /*****************************************
+    * Description: brief description of the methods purpose
+    * 
+    * Interface:
+    * 
+    * @param        each parameter of the method should be listed with an @param
+    * @param        parametername description of parameter
+    * 
+    * @return       any return value will be noted here
+    * ****************************************/
+    
+    
+    //*** Getters ***
+    
+    //*** Setters ***
+    
+} // end of public class
