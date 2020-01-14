d97944cf8e60edd51d2c8c1c90a0734bcbe9367b
minaker-l8 <minaker-l8@142.132.136.210>
2020-01-14 11:18:48 -0600

    First commit

---
+++ src/Time.java
@@ -0,0 +1,43 @@
+/********************************************************************
+ * Programmer:	Lauren
+ * 
+ * Class:  CS30S
+ *
+ * Assignment: Time class
+ *
+ * Description: time object
+ ***********************************************************************/
+
+// import libraries as needed here
+
+public class Time {
+    //*** Class Variables ***
+    
+        private int min = 0;
+        private int sec = 0;
+    
+    //*** Instance Variables ***
+    
+    //*** Constructors ***
+    
+    /*****************************************
+    * Description: brief description of the methods purpose
+    * 
+    * Interface:
+    * 
+    * @param        each parameter of the method should be listed with an @param
+    * @param        parametername description of parameter
+    * 
+    * @return       any return value will be noted here
+    * ****************************************/
+    
+        public Time(int min, int sec){
+            
+        }
+    
+    
+    //*** Getters ***
+    
+    //*** Setters ***
+    
+} // end of public class
