8c862c9f6938891ddb4854957b4b9020d65adc5e
minaker-l8 <minaker-l8@142.132.136.210>
2020-01-21 11:52:46 -0600

    code added

--- src/Time.java
+++ src/Time.java
@@ -43,12 +43,72 @@ public class Time {
     * 
     * Interface:
     * 
+    * @return      time: String, formatted string of the time
+    * 
     * ****************************************/
         public String getTime(){
             String time = (this.min + ":" + this.sec);
             return time;
         } // end getTime
         
+    /*****************************************
+    * Description: return the amount of minutes
+    * 
+    * Interface:
+    * 
+    * @return      min: int, number of minutes
+    * 
+    * ****************************************/
+        public int getMin(){
+            return this.min;
+        } // end getTime
+        
+    /*****************************************
+    * Description: return the amount of seconds
+    * 
+    * Interface:
+    * 
+    * @return      sec: int, number of seconds
+    * ****************************************/
+        public int getSec(){
+            return this.sec;
+        } // end getSec
+        
     //*** Setters ***
     
+    /*****************************************
+    * Description: change minutes and seconds of time
+    * 
+    * Interface:
+    * 
+    * @param      min: int, new amount of minutes
+    * @param      sec: int, new amount of seconds
+    * ****************************************/
+        public void setTime(int min, int sec){
+            this.min = min;
+            this.sec = sec;
+        } // end setTime
+        
+    /*****************************************
+    * Description: change minutes of time
+    * 
+    * Interface:
+    * 
+    * @param      min: int, new amount of minutes
+    * ****************************************/
+        public void setMin(int min){
+            this.min = min;
+        } // end setMin
+        
+    /*****************************************
+    * Description: change seconds of time
+    * 
+    * Interface:
+    * 
+    * @param      sec: int, new amount of seconds
+    * ****************************************/
+        public void setSec(int sec){
+            this.sec = sec;
+        } // end setMin
+        
 } // end of public class
