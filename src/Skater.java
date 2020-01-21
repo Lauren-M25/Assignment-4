118b8162126c0c417b3f4644495a092958764ead
minaker-l8 <minaker-l8@142.132.136.210>
2020-01-21 11:04:27 -0600

    code added

--- src/Skater.java
+++ src/Skater.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /********************************************************************
  * Programmer: Lauren
  * 
@@ -17,6 +20,9 @@ public class Skater {
     
     //*** Instance Variables ***
     
+        private int Id = 0;
+        private ArrayList<Time> times = new ArrayList();
+        
     //*** Constructors ***
     
     /*****************************************
@@ -30,9 +36,48 @@ public class Skater {
     * @return       any return value will be noted here
     * ****************************************/
     
-    
+    /*****************************************
+    * Description: Initialized constructor. Create a skater 
+    * object with an id and an array of times.
+    * 
+    * Interface:
+    * 
+    * @param       Skatertimes: String[], array of times from the text file
+    * 
+    * ****************************************/
+        public Skater(String[] Skatertimes){
+            this.Id = nextId++;
+            
+            int m = 0;
+            int s = 0;
+            
+            for(int i = 0; i < Skatertimes.length; i++){
+                
+                m = Integer.parseInt(Skatertimes[i]);
+                s = Integer.parseInt(Skatertimes[++i]);
+                
+                this.times.add(new Time (m,s));
+            } // end parse coordinates
+            
+        } // end initialized constructor
+        
     //*** Getters ***
     
+    /*****************************************
+    * Description: prints skater's id and times
+    * 
+    * Interface:
+    * 
+    * ****************************************/
+        
+        public void printInfo(){
+            System.out.println("ID: " + this.Id);
+            System.out.println("Times:");
+            for(int i = 0; i < this.times.size(); i++){
+                System.out.println(times.get(i).getTime());
+            }
+        }
+        
     //*** Setters ***
     
 } // end of public class
